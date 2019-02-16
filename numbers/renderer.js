6e1866edb795b87470eafd2931b64f6314f5516a
Jacob Bruinsma <me@jacobbruinsma.com>
2019-02-16 08:59:16 -0600

    A Roboto. Some refactoring, better chart. tableHoldings.js is now more a Component, less a View.

--- numbers/renderer.js
+++ numbers/renderer.js
@@ -1,3 +1,4 @@
+//#region info
 /*
 	This file is required by the index.html file and will
 	be executed in the renderer process for that window.
@@ -12,55 +13,70 @@
 	Key was: use oauth npm package, 
 	with version parameter '1.0A'
  */
+//#endregion
 
-// html without namespace
-require('./html.js')()
-var f = require('./format.js')
+require('./html.js')() // no namespace
+
+var format = require('./format.js')
 	, view = require('./view.js')
 
 var oauth = require('oauth')
 	, fs = require('fs')
 	, config = JSON.parse(fs.readFileSync('config.json'))
 	, apiEndpoint = 'https://api.tradeking.com/v1'
+	, apiStream = 'https://stream.tradeking.com/v1'
 	, url = {accounts: () => apiEndpoint + '/accounts.json'}
 	, ally = {accounts: undefined}
+	, requestTime = format.time(new Date())
 
-var consumer = new oauth.OAuth(
-	'https://developers.tradeking.com/oauth/request_token',
-	'https://developers.tradeking.com/oauth/access_token',
-	config.oauth.consumerKey,
-	config.oauth.consumerSecret,
-	'1.0A',
-	null,
-	'HMAC-SHA1')
 	
-function printAccount(accounts) {
+function printAccount(accounts) 
+{
 	var s = accounts.accountsummary
 		, holdings = s.accountholdings.holding
 	
 	tableHoldings = require('./tableHoldings.js')
 	view.setHeader(requestTime)
-	view.setContent('content', tableHoldings(f, holdings))
+	view.setContent('content', tableHoldings(format, holdings))
 }
 
-var requestTime
-setInterval(()=>{
-	requestTime = f.time(new Date())
-	consumer.get(url.accounts(),
-		config.oauth.oauthToken,
-		config.oauth.oauthTokenSecret,
-		(error, data, response) => {
-			if (error) {
-				console.log(error)
-			} else {
-				ally.accounts = JSON.parse(data).response.accounts
-				ally.accounts.time = requestTime
-				printAccount(ally.accounts)
-			}
-		})
-}, 5000)
+if (false) 
+{
+	var consumer = new oauth.OAuth(
+		'https://developers.tradeking.com/oauth/request_token',
+		'https://developers.tradeking.com/oauth/access_token',
+		config.oauth.consumerKey,
+		config.oauth.consumerSecret,
+		'1.0A',
+		null,
+		'HMAC-SHA1')
 
-// //test with a static file:
-// var accountsRsp = JSON.parse(fs.readFileSync('accounts.json')).response.accounts
-// accountsRsp.time = requestTime
-// printAccount(accountsRsp)
+	setInterval(() => 
+	{
+		requestTime = format.time(new Date())
+		consumer.get(
+			url.accounts(),
+			config.oauth.oauthToken,
+			config.oauth.oauthTokenSecret,
+			(error, data, response) => 
+			{
+				if (error) 
+				{
+					console.log(error)
+				} 
+				else 
+				{
+					ally.accounts = JSON.parse(data).response.accounts
+					ally.accounts.time = requestTime
+					printAccount(ally.accounts)
+				}
+			})
+	}, 5000)
+} 
+else 
+{
+	//test with a static file:
+	var accountsRsp = JSON.parse(fs.readFileSync('accounts.json')).response.accounts
+	accountsRsp.time = requestTime
+	printAccount(accountsRsp)
+}
--- numbers/tableHoldings.js
+++ numbers/tableHoldings.js
@@ -1,25 +1,31 @@
-module.exports = (f, holdingsArray) => {
-	thead = `
+var f
+	, cost = holding =>
+		Number.parseFloat(holding.costbasis) / Number.parseFloat(holding.qty)
+	, thead = `
 		<thead>
 			<tr>
 				<th class="mdl-data-table__cell--non-numeric">Symbol</th>
+				<th>Cost basis</th>
 				<th>Last price</th>
+				<th>Shares</th>
 				<th>Gain/loss</th>
 			</tr>
 		</thead>`
-
-	tbody = (holdings) => `
+	, tbody = holdings => `
 		<tbody>
 			${holdings.map(h => row(h)).join('\n')}
 		</tbody>`
-
-	row = (holding) => `
+	, row = holding => `
 		<tr>
 			<td class="mdl-data-table__cell--non-numeric">${holding.displaydata.symbol}</td>
+			<td>${f.finan(cost(holding))}</td>
 			<td>${f.finan(holding.price)}</td>
+			<td>${holding.qty}</td>
 			<td>${f.finan(holding.gainloss)}</td>
 		</tr>`
 
+module.exports = (format, holdingsArray) => {
+	f = format
 	table = `
 		<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
 			${thead}
